c3827109db8dd73581dc95f9c07f06241c9bbf47
William Morriss <wjmelements@gmail.com>
2025-06-04 10:33:55 -0500

    rename Counter

--- test/AmericanOption.t.sol
+++ test/AmericanOption.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {AmericanOption} from "../src/AmericanOption.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract AmericanOptionTest is Test {
+    AmericanOption public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new AmericanOption();
         counter.setNumber(0);
     }
 
