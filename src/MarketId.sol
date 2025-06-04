1d66f285f2a15318fcacda3b124094b3e1f08e3f
William Morriss <wjmelements@gmail.com>
2025-06-04 11:49:02 -0500

    test MarketId pack/unpack

--- src/MarketId.sol
+++ src/MarketId.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.13;
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
 library MarketId {
-    uint256 private constant EXPIRY_MASK = 0x000000000000000000ffffff0000000000000000000000000000000000000000;
+    uint256 private constant KEY_EXPIRY_MASK = 0x000000000000000000ffffff0000000000000000000000000000000000000000;
     // expiry: lower 2 bytes are zero
 
     function unpack(uint256 key) internal pure returns (IERC20 token, uint256 expiry, uint256 strike) {
         token = IERC20(address(uint160(key)));
-        expiry = (key & EXPIRY_MASK) >> 144;
+        expiry = (key & KEY_EXPIRY_MASK) >> 144;
         strike = key >> 184;
     }
     function pack(IERC20 token, uint256 expiry, uint256 strike) internal pure returns (uint256 key) {
---
+++ test/MarketId.t.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.13;
+
+
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {MarketId} from "../src/MarketId.sol";
+import {Expiry} from "../src/Expiry.sol";
+import {TestUSD} from "./TestUSD.sol";
+
+contract AmericanOptionsTest is Test {
+    TestUSD usd;
+    using MarketId for uint256;
+    using Expiry for uint256;
+
+    function setUp() public {
+        usd = new TestUSD();
+    }
+
+    function test_USD_pack_unpack() public view {
+        uint256 expiry = block.timestamp.toExpiry();
+        assertTrue(expiry.isValidExpiry());
+        uint256 strike = 3_00;
+        uint256 marketId = MarketId.pack(usd, expiry, strike);
+        (IERC20 tokenDecoded, uint256 expiryDecoded, uint256 strikeDecoded) = marketId.unpack();
+        assertEq(address(tokenDecoded), address(usd));
+        assertEq(strikeDecoded, strike);
+        assertEq(expiryDecoded, expiry);
+    }
+
+    function test_USD_unpack_pack() public view {
+        uint256 marketId = 0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef;
+        (IERC20 token, uint256 expiry, uint256 strike) = marketId.unpack();
+        uint256 marketIdDecoded = MarketId.pack(token, expiry, strike);
+        assertEq(marketIdDecoded, marketId); 
+    }
+}
