338afd0aae962a8a58afe63a70755199013149c0
William Morriss <wjmelements@gmail.com>
2025-06-04 21:17:45 -0500

    fix fuzz test case

--- test/Operator.t.sol
+++ test/Operator.t.sol
@@ -100,8 +100,12 @@ contract AmericanOptions7702OperatorTest is Test {
             operatorAlice.closeAndWithdrawExpiredPositionPreferringCollateral(marketId);
 
             options.transfer(purchaser, marketId, 1000_00);
-            assertEq(options.balanceOf(purchaser, marketId), 1000_00);
-            assertEq(options.balanceOf(alice, marketId), 0);
+            if (alice != purchaser) {
+                assertEq(options.balanceOf(purchaser, marketId), 1000_00);
+                assertEq(options.balanceOf(alice, marketId), 0);
+            } else {
+                assertEq(options.balanceOf(purchaser, marketId), 1000_00);
+            }
         }
         vm.stopPrank();
 
